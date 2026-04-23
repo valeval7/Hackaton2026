ab8fea3ce5b40e12bb1debc61089b6f4ba9c4702
Valeria Gonzalez <valeria._mg@hotmail.com>
2026-04-23 02:08:54 -0600

    logo

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
 <div class="shrink-0 flex items-center">
     <a href="{{ route('dashboard') }}">
         <img src="https://raw.githubusercontent.com/valeval7/Hackaton2026/352647689f6ea1a0478b526ceceda251b54f6569/logo2.svg"
-            class="block h-14 w-auto" alt="Logo">
+            class="block h-16 w-auto" alt="Logo">
     </a>
 </div>
 
