c4582849d4f018bb011b5bed19da3486bbafc967
Valeria Gonzalez <valeria._mg@hotmail.com>
2026-04-23 02:28:38 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 use App\Livewire\GoalTracker;
 use App\Livewire\TaskList;
 use App\Livewire\ListaNotificacion;
+use App\Livewire\TaskCreate;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,7 +18,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-
+    
+    Route::get('/tareas/crear', TaskCreate::class)->name('tasks.create');
     Route::get('/tareas', TaskList::class)->name('tasks.index');
     Route::get('/metas', GoalTracker::class)->name('goals.index');
     Route::get('/notificaciones', ListaNotificacion::class)->name('notifications.index');
