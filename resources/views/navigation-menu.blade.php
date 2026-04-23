352647689f6ea1a0478b526ceceda251b54f6569
Valeria Gonzalez <valeria._mg@hotmail.com>
2026-04-23 02:06:07 -0600

    logo

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,10 +2,10 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-<!-- Logo -->
+                <!-- Logo -->
 <div class="shrink-0 flex items-center">
     <a href="{{ route('dashboard') }}">
-        <img src="https://github.com/valeval7/Hackaton2026/blob/main/logo2.svg?raw=true"
+        <img src="https://github.com/valeval7/Hackaton2026/blob/main/logo_tareas2.png?raw=true"
             class="block h-14 w-auto" alt="Logo">
     </a>
 </div>
