75320ac47f59c9134e2684345d8ae44ef280cd44
Valeria Gonzalez <valeria._mg@hotmail.com>
2026-04-23 02:10:12 -0600

    nav

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-[#192338] border-b border-[#31487A]">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-25">
             <div class="flex">
                 <!-- Logo -->
 <div class="shrink-0 flex items-center">
